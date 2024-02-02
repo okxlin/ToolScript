580e742209792273b6d1bd81bbd0c7188548d42b
okxlin <61420215+okxlin@users.noreply.github.com>
2024-02-03 05:19:51 +0800

    Update 1panel_docker_to_sys.sh

--- 1Panel/1panel-execution-mode/1panel_docker_to_sys.sh
+++ 1Panel/1panel-execution-mode/1panel_docker_to_sys.sh
@@ -18,8 +18,8 @@ check_1panel_installed() {
     echo "1Panel 尚未在宿主机上安装，继续检查数据库文件。"
     
     read -p "请输入 1Panel 数据所在的顶层目录路径（默认为 /opt）: " db_directory
-    db_directory=${db_directory:-"/opt"}
-    local db_file="$db_directory/1panel/db/1Panel.db"
+    db_directory="${db_directory:-"/opt"}"
+    local db_file="${db_directory}/1panel/db/1Panel.db"
     
     if [[ ! -f "$db_file" ]]; then
         echo "1Panel 未安装过，不需要执行迁移"
@@ -27,9 +27,9 @@ check_1panel_installed() {
     else
         # 备份数据库文件
         local backup_dir="/opt/1panel-bak/db"
-        mkdir -p "$backup_dir"
-        cp "$db_file" "$backup_dir/1Panel.db"
-        echo "已经备份旧数据库文件到 $backup_dir/1Panel.db"
+        mkdir -p "${backup_dir}"
+        cp "$db_file" "${backup_dir}/1Panel.db"
+        echo "已经备份旧数据库文件到 ${backup_dir}/1Panel.db"
     fi
 }
 
@@ -63,58 +63,58 @@ check_and_install_dependencies() {
 
 # 函数：创建目录
 create_dir() {
-    PANEL_DIR=~/1panel-install-dir
-    mkdir -p ${PANEL_DIR}
-    cd ${PANEL_DIR}
+    local panel_dir=~/1panel-install-dir
+    mkdir -p "${panel_dir}"
+    cd "${panel_dir}" || { echo "无法进入目录 ${panel_dir}，脚本无法继续执行。"; exit 1; }
 }
 
 # 函数：下载 1Panel
 download_1panel() {
-    local osCheck=$(uname -a)
-    local INSTALL_MODE=${1:-"stable"}
+    local os_check=$(uname -a)
+    local install_mode=${1:-"stable"}
     
-    if [[ $osCheck =~ 'x86_64' ]]; then
+    if [[ $os_check =~ 'x86_64' ]]; then
         local architecture="amd64"
-    elif [[ $osCheck =~ 'arm64' ]] || [[ $osCheck =~ 'aarch64' ]]; then
+    elif [[ $os_check =~ 'arm64' ]] || [[ $os_check =~ 'aarch64' ]]; then
         local architecture="arm64"
-    elif [[ $osCheck =~ 'armv7l' ]]; then
+    elif [[ $os_check =~ 'armv7l' ]]; then
         local architecture="armv7"
-    elif [[ $osCheck =~ 'ppc64le' ]]; then
+    elif [[ $os_check =~ 'ppc64le' ]]; then
         local architecture="ppc64le"
-    elif [[ $osCheck =~ 's390x' ]]; then
+    elif [[ $os_check =~ 's390x' ]]; then
         local architecture="s390x"
     else
         echo "暂不支持的系统架构，请参阅官方文档，选择受支持的系统。"
         return 1
     fi
 
-    if [[ ${INSTALL_MODE} != "dev" && ${INSTALL_MODE} != "stable" ]]; then
+    if [[ ${install_mode} != "dev" && ${install_mode} != "stable" ]]; then
         echo "请输入正确的安装模式（dev or stable）"
         return 1
     fi
 
-    local VERSION=$(curl -s https://resource.fit2cloud.com/1panel/package/${INSTALL_MODE}/latest)
-    local FIT2CLOUD_URL="https://resource.fit2cloud.com/1panel/package/${INSTALL_MODE}/${VERSION}/release/checksums.txt"
-    local GITHUB_URL="https://github.com/wanghe-fit2cloud/1Panel/releases/download/${VERSION}/checksums.txt"
+    local version=$(curl -s https://resource.fit2cloud.com/1panel/package/${install_mode}/latest)
+    local fit2cloud_url="https://resource.fit2cloud.com/1panel/package/${install_mode}/${version}/release/checksums.txt"
+    local github_url="https://github.com/wanghe-fit2cloud/1Panel/releases/download/${version}/checksums.txt"
 
-    if [[ "x${VERSION}" == "x" ]]; then
+    if [[ "x${version}" == "x" ]]; then
         echo "获取最新版本失败，请稍候重试"
         return 1
     fi
 
-    local package_file_name="1panel-${VERSION}-linux-${architecture}.tar.gz"
+    local package_file_name="1panel-${version}-linux-${architecture}.tar.gz"
 
     local fit2cloud_latency=$(ping -c 3 -q -w 2 resource.fit2cloud.com | awk -F'/' 'END{print $5}')
     local github_latency=$(ping -c 3 -q -w 2 github.com | awk -F'/' 'END{print $5}')
 
     local package_download_url=""
     if [[ $(echo "$fit2cloud_latency < $github_latency" | bc) -eq 1 ]]; then
-        package_download_url="https://resource.fit2cloud.com/1panel/package/${INSTALL_MODE}/${VERSION}/release/${package_file_name}"
+        package_download_url="https://resource.fit2cloud.com/1panel/package/${install_mode}/${version}/release/${package_file_name}"
     else
-        package_download_url="https://github.com/wanghe-fit2cloud/1Panel/releases/download/${VERSION}/${package_file_name}"
+        package_download_url="https://github.com/wanghe-fit2cloud/1Panel/releases/download/${version}/${package_file_name}"
     fi
 
-    echo "开始下载 1Panel ${VERSION} 版本在线安装包"
+    echo "开始下载 1Panel ${version} 版本在线安装包"
     echo "选用下载源："
     if [[ $(echo "$fit2cloud_latency < $github_latency" | bc) -eq 1 ]]; then
         echo "resource.fit2cloud.com"
@@ -123,26 +123,29 @@ download_1panel() {
     fi
     echo "安装包下载地址： ${package_download_url}"
 
-    curl -LOk -o ${package_file_name} ${package_download_url}
+    curl -LOk -o "${package_file_name}" "${package_download_url}"
     if [ ! -f ${package_file_name} ]; then
         echo "下载安装包失败，请稍候重试。"
-        rm -f ${package_file_name}
-        return 1
+        cd ~
+        rm -rf "${panel_dir}"
+        exit 1
     fi
-    cd ${PANEL_DIR}
-    tar zxvf ${package_file_name} --strip-components 1
+    cd "${panel_dir}" || { echo "无法进入目录 ${panel_dir}，脚本无法继续执行。"; exit 1; }
+    tar zxvf "${package_file_name}" --strip-components 1
     if [ $? != 0 ]; then
         echo "解压安装包失败，请稍候重试。"
-        rm -f ${package_file_name}
-        return 1
+        cd ~
+        rm -rf "${panel_dir}"
+        exit 1
     fi
 }
 
 # 函数：安装 1Panel
 install_1panel() {
-    cp ${PANEL_DIR}/1panel /usr/local/bin && chmod +x /usr/local/bin/1panel
-    cp ${PANEL_DIR}/1pctl /usr/local/bin && chmod +x /usr/local/bin/1pctl
-    cp ${PANEL_DIR}/1panel.service /etc/systemd/system
+    local panel_dir=~/1panel-install-dir
+    cp "${panel_dir}/1panel" /usr/local/bin && chmod +x /usr/local/bin/1panel
+    cp "${panel_dir}/1pctl" /usr/local/bin && chmod +x /usr/local/bin/1pctl
+    cp "${panel_dir}/1panel.service" /etc/systemd/system
     if [[ ! -f /usr/bin/1panel ]]; then
         ln -s /usr/local/bin/1panel /usr/bin/1panel >/dev/null 2>&1
     fi
@@ -152,11 +155,12 @@ install_1panel() {
     systemctl daemon-reload
     systemctl enable 1panel
     systemctl start 1panel
-    sleep 2 # 等待服务启动
+    echo "等待服务启动中…"
+    sleep 8
     if systemctl status 1panel >/dev/null 2>&1; then
-        echo "1panel 服务已成功启动。"
+        echo "1Panel 服务已成功启动。"
     else
-        echo "1panel 服务启动失败。"
+        echo "1Panel 服务启动失败。"
     fi
 }
 
@@ -169,8 +173,8 @@ remove_container() {
         echo "已创建的 Docker 容器及其对应镜像："
         docker ps -a --format "table {{.Names}}\t{{.Image}}"
 
-        read -p "请输入要移除的 1Panel 容器名（默认为 '1panel'，输入 'removed' 则表示已经手动移除过）: " container_name
-        container_name=${container_name:-"1panel"}
+        read -p "请输入要移除的 1Panel 容器名（默认为 '1panel'；输入 'removed' 则表示已经手动移除过，将跳过移除）: " container_name
+        container_name="${container_name:-"1panel"}"
 
         if [[ $container_name != "removed" ]]; then
             if docker inspect "$container_name" &>/dev/null; then
@@ -188,11 +192,11 @@ remove_container() {
     fi
 }
 
-
 # 函数：返回用户根目录并删除临时文件
 cleanup_and_notify() {
-    local user_home=$(eval echo ~$USER)
-    rm -rf ${PANEL_DIR}
+    local panel_dir=~/1panel-install-dir
+    cd ~
+    rm -rf "${panel_dir}"
     echo "1Panel 已经由 Docker 运行方式切换到宿主机直接运行"
 }
 
